4517d919c38f4d9502176dac399888b9517f1b41
Roman Kis <roman.kiss.scuola@gmail.com>
2022-12-10 08:58:57 +0100

    update

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
 import { Subject } from "rxjs";
 import { AuthData } from "./auth-data.model";
 
@@ -24,13 +25,15 @@ export class AuthService{
         return this.isAuthenticated;
     }
     
-    constructor(private http: HttpClient ){
+    constructor(private http: HttpClient, private router: Router ){
 
     }
 createUser(email: string, password: string){
     const authData : AuthData = {email: email, password: password};
     this.http.post("http://localhost:3000/api/user/signup", authData).subscribe(response => {
         console.log(response);
+        this.router.navigate(["/login"]);
+
     });
 
 }
@@ -41,9 +44,10 @@ createUser(email: string, password: string){
             this.token = token;
             if(token){
                 this.isAuthenticated = true;
-
             this.authStatusListener.next(true);//we are emitting a value of true, so any component that is listening to this observable will be notified
-    }});
+            this.router.navigate(["/"]);
+    }
+});
 
 
 
@@ -54,5 +58,7 @@ createUser(email: string, password: string){
         this.token = null;
         this.isAuthenticated = false;
         this.authStatusListener.next(false);
+        this.router.navigate(["/"]);
+
     }
 }
